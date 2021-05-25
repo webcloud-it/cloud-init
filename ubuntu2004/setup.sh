6b2b3bbd47700f7bdfdcd4177d7e78dac043d065
Matteo Gazzoni <matteo.gazzoni@webcloud.it>
2021-05-25 17:03:27 +0200

    Update setup.sh

--- ubuntu2004/setup.sh
+++ ubuntu2004/setup.sh
@@ -2,8 +2,13 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
+# ssh
+sed -i 's/[#]*PermitRootLogin yes/PermitRootLogin prohibit-password/g' /etc/ssh/sshd_config
+sed -i 's/[#]*PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+service sshd restart
+
 # docker
-cat > /etc/docker/daemon.json <<"EOF"
+cat >/etc/docker/daemon.json <<"EOF"
 {
 	"log-driver": "json-file",
 	"log-opts": {
@@ -16,19 +21,19 @@ EOF
 bash <(curl -Ss -L https://releases.rancher.com/install-docker/20.10.sh)
 
 # date and time
-apt update
-apt install -yq --no-install-recommends ntp
+apt-get update
+apt-get install -yq --no-install-recommends ntp
 timedatectl set-timezone Europe/Rome
 timedatectl set-ntp false
 service ntp restart
 
 # dns
-apt update
-apt install -yq --no-install-recommends resolvconf
-echo nameserver 1.1.1.1 > /etc/resolvconf/resolv.conf.d/head
-echo nameserver 8.8.8.8 >> /etc/resolvconf/resolv.conf.d/head
+apt-get update
+apt-get install -yq --no-install-recommends resolvconf
+echo nameserver 1.1.1.1 >/etc/resolvconf/resolv.conf.d/head
+echo nameserver 8.8.8.8 >>/etc/resolvconf/resolv.conf.d/head
 resolvconf -u
 
 # misc
-echo 'set background=dark' >> /root/.vimrc
+echo 'set background=dark' >>/root/.vimrc
 rm /etc/apt/apt.conf.d/20auto-upgrades
